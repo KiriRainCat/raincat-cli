5e000ed229d70e0ba6d5c4db469d1cdc46ffc388
KiriRainCat <2287990465@qq.com>
2023-07-29 07:15:08 +0800

    ✨ feat: 添加下载加载动画与当前目录创建项目

--- bin/lib/download.js
+++ bin/lib/download.js
@@ -1,6 +1,8 @@
 import chalk from "chalk";
 import download from "download-git-repo";
 import fs from "fs";
+import ora from "ora";
+import path from "path";
 import process from "process";
 
 /**
@@ -11,9 +13,10 @@ import process from "process";
  * @param {boolean} force 强制？
  */
 const downloadRepo = (name, template, force) => {
-  const dest = process.cwd() + "\\" + name;
+  const __dirname = process.cwd();
+  let dest = __dirname + "\\" + name;
   // 如果路径存在
-  if (fs.existsSync(dest)) {
+  if (fs.existsSync(dest) && name !== ".") {
     if (!force) {
       console.log(
         `${chalk.red("ERR!")} ${name} 目录已存在，如需覆盖，请使用 ${chalk.cyan(
@@ -22,21 +25,27 @@ const downloadRepo = (name, template, force) => {
       );
       return;
     }
-    fs.rmdirSync(dest);
+    fs.rmSync(dest, { force: true, recursive: true });
   }
 
-  // 如果与当前路径同名
-  if (process.cwd().endsWith(name)) {
-    dest = process.cwd();
+  // 如果与当前路径同名 或 项目名 = "."
+  if (__dirname.endsWith(name) || name === ".") {
+    dest = __dirname;
+    name = path.basename(__dirname);
   }
 
   // 仓库下载模板
+  const spinner = ora({ spinner: "line", suffixText: "downloading..." });
+  spinner.start();
   download(template.repo, dest, (err) => {
     // 报错展示下载失败
     if (err) {
-      console.log("下载失败...");
+      console.log(chalk.bold.red("\n ❌  下载失败！"));
       return;
     }
+
+    spinner.stop();
+    console.log(chalk.bold.green("\n ✔️  下载成功！"));
 
     // 如果是后端项目，不展示 pnpm install 等提示
     if (!template.isFrontend) {
@@ -47,7 +56,7 @@ const downloadRepo = (name, template, force) => {
     // 前端项目，展示 pnpm install 等提示
     console.log(`\r\nSuccessfully created project ${chalk.cyan(name)}`);
     console.log(`\r\n  cd ${chalk.cyan(name)}`);
-    console.log("  pnpm install\r\n");
+    console.log("  pnpm install");
     console.log("  pnpm run dev\r\n");
   });
 };
