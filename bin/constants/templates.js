e12f71b28ab41dbb0bc56ea60a4546e2aa18cdcd
KiriRainCat <2287990465@qq.com>
2023-07-28 20:23:08 +0800

    ✨ feat: 添加一个template

--- bin/constants/templates.js
+++ bin/constants/templates.js
@@ -1,9 +1,16 @@
 export default [
   {
+    name: "vue3-template-lite",
+    value: {
+      isFrontend: true,
+      repo: "github:KiriRainCat/vue3-template#lite",
+    },
+  },
+  {
     name: "vue3-template",
     value: {
       isFrontend: true,
-      repo: "github:KiriRainCat/vue3-template#main",
+      repo: "github:KiriRainCat/vue3-template",
     },
   },
   {
