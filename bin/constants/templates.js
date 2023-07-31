9b25ff9223da670a61bb3c49784b2547a9fae9e9
KiriRainCat <2287990465@qq.com>
2023-07-31 15:23:29 +0800

    🐳 chore: 模板显示优先级调换

--- bin/constants/templates.js
+++ bin/constants/templates.js
@@ -7,17 +7,17 @@ export default [
     },
   },
   {
-    name: "vue3-template",
-    value: {
-      isFrontend: true,
-      repo: "github:KiriRainCat/vue3-template",
-    },
-  },
-  {
     name: "go-template",
     value: {
       ifFrontend: false,
       repo: "github:KiriRainCat/go-template",
+    },
+  },
+  {
+    name: "vue3-template",
+    value: {
+      isFrontend: true,
+      repo: "github:KiriRainCat/vue3-template",
     },
   },
 ];
